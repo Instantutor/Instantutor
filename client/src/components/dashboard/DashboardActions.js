2c58ee7236776e41d0c77a831f2a83b4fb749577
Setsurinne <xjx1525392406@gmail.com>
2021-06-20 17:05:07 -0400

    Comment some unnecessary code
    Some unnecessary code were left from last semester, so maybe free to comment/delete them

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom';
-import formData from '../profile-forms/ProfileForm'
+//import formData from '../profile-forms/ProfileForm'
 
 
 const DashboardActions = () => {
