777b03a66c2da70e38e49f61fb9b8f70b2fa4a34
TylerBaughcome <tbaughcome@gmail.com>
2021-06-25 12:18:43 -0400

    Refined request model to take array of requests for every user. Limit for user requests now based on size of array in database

--- routes/api/request.js
+++ routes/api/request.js
@@ -19,34 +19,6 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    //Upper Bound Request Checking...
-    try {
-      var user = await User.findOne({ _id: req.user.id });
-      //check if user exists
-      if (user != null) {
-        if (user.currentRequests < 5) {
-          user = await User.findOneAndUpdate(
-            { _id: req.user.id },
-            { $inc: { currentRequests: 1 } },
-            { new: true }
-          );
-          user.save();
-        } else {
-          console.error(
-            "User cannot submit more than 5 concurrent requests for help."
-          );
-          return res.json({
-            msg: "User may only have 5 concurrent requests for help out at the same time.",
-          });
-        }
-      } else {
-        console.error("User does not exist");
-        return res.status(404).json({ error: "User does not exist" });
-      }
-    } catch (err) {
-      console.error("Server error", err);
-      return res.status(400).json({ msg: "Could not increment requests." });
-    }
     const {
       request,
       course,
@@ -56,10 +28,7 @@ router.post(
       availability,
       number_sessions,
     } = req.body;
-
     const requestFields = {};
-    requestFields.name = req.user.name;
-    requestFields.user = req.user.id;
     if (request) requestFields.request = request;
     if (course) requestFields.course = course;
     if (grade) requestFields.grade = grade;
@@ -67,15 +36,56 @@ router.post(
     if (help_time) requestFields.help_time = help_time;
     if (availability) requestFields.availability = availability;
     if (number_sessions) requestFields.number_sessions = number_sessions;
+    const requestByUser = await Request.findOne({ user: req.user.id });
+    if (!requestByUser) {
+      //initialize new set of requests for user
 
-    try {
-      //Adds new request to requests collection
-      let newRequest = new Request(requestFields);
-      await newRequest.save();
-      return res.json({ msg: "Request added.", request: newRequest });
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
+      const requestData = {};
+      requestData.name = req.user.name;
+      requestData.user = req.user.id;
+      var requestArr = [];
+      requestArr.push(requestFields);
+      requestData.requests = requestArr;
+
+      try {
+        //Adds new request to requests collection
+        let newRequest = new Request(requestData);
+        await newRequest.save();
+        console.log(
+          "Requests for user initialized as user did not exists or had no active requests."
+        );
+        return res.json({
+          msg: "Requests for user initialized.",
+          request: newRequest,
+        });
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+      }
+    } else {
+      try {
+        //TODO: need to check for upper bound...
+        if (requestByUser.requests.length < 5) {
+          let originalRequests = await Request.findOneAndUpdate(
+            { user: req.user.id },
+            { $push: { requests: requestFields } }
+          );
+          res.json({
+            msg: "Request added for user.",
+            "Original requests": originalRequests,
+            newRequest: requestFields,
+          });
+        } else {
+          console.error("User cannot exceed maximum of 5 concurrent requests.");
+          res.status(400).json({
+            error:
+              "User tried to exceed maximum of 5 concurrent requests for help.",
+          });
+        }
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error (adding)");
+      }
     }
   }
 );
