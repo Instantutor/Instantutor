56105b891166eeec486936f221aab9820daa48d2
Setsurinne <xjx1525392406@gmail.com>
2021-08-12 20:40:29 -0400

    Rewrite part of code in matched tutor page
    Now the user can check tutor's response and make final decision directly in this page

--- client/src/components/user-requests/MatchedTutorItem.js
+++ client/src/components/user-requests/MatchedTutorItem.js
@@ -1,71 +1,76 @@
 import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { disperseToTutors } from "../../actions/request";
+import { connect } from "react-redux";
 
-const defaultConfirm = {
-  "backgroundColor": "#f2f2f2",
-  color: "black",
-};
-const clickedConfirm = {
-  "backgroundColor": "#17a2b8",
-  color: "white",
-};
 
-class MatchedTutorItem extends React.Component {
-  isConfirmed = () => this.state.clicked;
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentStyle: this.props.confirmed ? clickedConfirm: defaultConfirm,
-      clicked: this.props.confirmed,
-    };
-  }
-  render() {
-    const tutor = this.props;
-    const confirmButtonClicked = () => {
-      const newStyle =
-        this.state.currentStyle === defaultConfirm
-          ? clickedConfirm
-          : defaultConfirm;
-      this.setState({ currentStyle: newStyle });
-      this.setState({ clicked: !this.state.clicked });
-      //dispatch to add tutor to list of tutors to confirm for user
-    };
-
-    let { name, _id, avatar, bio } = tutor.item;
-    return (
-      <div className="profile bg-white p-2">
-        <img src={avatar} alt="" className="round-img" />
-        <div>
-          <h2>{name}</h2>
-          {bio && (
-            <p>
-              <i className="far fa-address-card"> {" " + bio} </i>
-            </p>
-          )}
-        </div>
-
-        <Link
-          to={`/profile/${_id}`}
-          target="_blank"
-          className="btn btn-primary"
-        >
-          <i className="fas fa-user-circle"> </i> View Profile
-        </Link>
-        <button
-          className="btn-confirm"
-          style={this.state.currentStyle}
-          onClick={confirmButtonClicked}
-        >
-          {this.state.clicked ? "Unconfirm" : "Confirm"}
-        </button>
+const MatchedTutorItem = ({
+  item: {
+    name, _id, avatar, bio, state
+  },
+  req_id,
+  disperseToTutors,
+}) => {
+  return (
+    <div className="profile bg-white p-2">
+      <img src={avatar} alt="" className="round-img" />
+      <div>
+        <h2>{name}</h2>
+        {bio && (
+          <p>
+            <i className="far fa-address-card"> {" " + bio} </i>
+          </p>
+        )}
       </div>
-    );
-  }
+
+      <Link
+        to={`/profile/${_id}`}
+        target="_blank"
+        className="btn btn-primary"
+      >
+        <i className="fas fa-user-circle"> </i> View Profile
+      </Link>
+      
+      <Fragment>
+        {state === "UNSEND" ? (
+          <button
+            className="btn-confirm"
+            onClick={() => {
+              disperseToTutors([_id], req_id)}
+            }
+          >
+            Send request
+          </button>
+        ) : (
+          <div>
+            <i> The tutor's state: </i>  
+            <i className="text-primary">  {state} </i>
+          </div>
+        )}
+        
+        {
+          state === "ACCEPT" && (
+            <button
+              onClick={function () {
+                // To be implemented: Should call finalize method and dispatch to a new page for tutor.
+              }}
+              className="btn btn-success"
+            >
+              Finalize Tutor
+            </button>
+          )
+        }
+      </Fragment>
+    </div>
+  );
 }
+
 
 MatchedTutorItem.propTypes = {
   tutor: PropTypes.object,
 };
 
-export default MatchedTutorItem;
+export default connect(null, {
+  disperseToTutors,
+})(MatchedTutorItem);
--- client/src/components/user-requests/UserRequestMatchedTutor.js
+++ client/src/components/user-requests/UserRequestMatchedTutor.js
@@ -9,13 +9,11 @@ import MatchedTutorItem from "./MatchedTutorItem";
 const UserRequestMatchedTutor = ({
   user,
   match,
-  requests: { request_history, loading, confirmed_tutors },
+  requests: { request_history, loading },
   getRequestHistory,
-  disperseToTutors,
 }) => {
-  let request_id = match.params.id;
+  const request_id = match.params.id;
   const [tutors, setTutorsData] = useState(null);
-  const [requestId, setRequestId] = useState(null);
 
   useEffect(async () => {
     (await user) && getRequestHistory(user._id);
@@ -28,7 +26,6 @@ const UserRequestMatchedTutor = ({
           .map((item) => item._id)
           .indexOf(request_id);
         setTutorsData(request_history[request_index].potential_tutors);
-        setRequestId(request_history[request_index]._id);
       }
     }
   }, [loading, request_history]);
@@ -62,42 +59,13 @@ const UserRequestMatchedTutor = ({
       </Fragment>
     );
   } else {
-    const tutorRefs = [];
-    for (var i in tutors) {
-      var ref = React.createRef();
-      tutorRefs.push(ref);
-    }
-    var i = 0;
-    //set up references to components
-    var tutorsComponent = [];
-    tutors.forEach((tutor) => {
-      const tutorRef = tutorRefs[i];
-      var isConfirmed = false;
-      //check confirmed tutors for this requests using confirmed_tutors object
-      if (
-        confirmed_tutors &&
-        confirmed_tutors[request_id] &&
-        confirmed_tutors[request_id].includes(tutor._id)
-      ) {
-        isConfirmed = true;
-      }
-      const component = (
-        <MatchedTutorItem
-          ref={tutorRef}
-          key={tutor._id}
-          item={tutor}
-          confirmed={isConfirmed}
-        />
-      );
-      tutorsComponent.push(component);
-      i++;
-    });
-
     return (
       <Fragment>
         <div className="profiles">
           <h1 className="large text-primary">Check your tutors!</h1>
-          {tutorsComponent}
+            {tutors.map(tutor => (
+              <MatchedTutorItem key={tutor._id} item={tutor} req_id={request_id}/>
+            ))}
         </div>
         <button
           onClick={() => window.history.back(-1)}
@@ -108,21 +76,7 @@ const UserRequestMatchedTutor = ({
         <Link to="/dashboard" className="btn btn-light">
           Back To Dashboard
         </Link>
-        <Link
-          onClick={() => {
-            var tutor_ids = {};
-            //true => tutor confirmed
-            for (var i in tutorRefs) {
-              const tutor_id = tutorRefs[i].current.props.item._id;
-              tutor_ids[tutor_id] = tutorRefs[i].current.isConfirmed();
-            }
-            disperseToTutors(tutor_ids, requestId);
-          }}
-          className="btn btn-primary"
-          style={{ float: "right" }}
-        >
-          Submit
-        </Link>
+      
       </Fragment>
     );
   }
