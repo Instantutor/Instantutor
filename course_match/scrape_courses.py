e2f2d76f309d5a5a5c38826fe86fc03da8ca5b4f
Samyuth <samyuth.sagi@gmail.com>
2022-03-23 03:47:36 -0400

    Request courselist integration
    Initial frontend modification and changes to the scraper

--- course_match/scrape_courses.py
+++ course_match/scrape_courses.py
@@ -32,7 +32,7 @@ def main():
     print("Starting")
     scraped = dict()
     
-    with open("courses.json", "r") as file:
+    with open("../server/config/courses.json", "r") as file:
         scraped = json.load(file)
     
     with open("subj_and_codes.json", "w") as file:
@@ -44,7 +44,22 @@ def main():
         subjects = [scraped[i]["name"] for i in range(len(scraped))]
         #subjdict = [{"name": scraped[i]["name"], "code": scraped[i]["code"]} for i in range(len(scraped))]
         file.write(json.dumps(subjects))
+
+    with open("subj_and_courses.json", "w") as file:
+        final_list = {"subject_list": [scraped[i]["name"] for i in range(len(scraped))]}
+        final_list["course_list"] = {}
+        for i in range(len(scraped)):
+            final_list["course_list"][scraped[i]["name"]] = [scraped[i]["courses"][j]["title"] for j in range(len(scraped[i]["courses"]))]
+        #subjdict = [{"name": scraped[i]["name"], "code": scraped[i]["code"]} for i in range(len(scraped))]
+        file.write(json.dumps(final_list))
         
+    with open("../client/src/course_list.json", "w") as file:
+        final_list = {"subject_list": [scraped[i]["name"] for i in range(len(scraped))]}
+        final_list["course_list"] = {}
+        for i in range(len(scraped)):
+            final_list["course_list"][scraped[i]["name"]] = [scraped[i]["courses"][j]["title"] for j in range(len(scraped[i]["courses"]))]
+        #subjdict = [{"name": scraped[i]["name"], "code": scraped[i]["code"]} for i in range(len(scraped))]
+        file.write(json.dumps(final_list))
     
     return scraped
     
