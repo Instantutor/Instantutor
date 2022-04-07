1c25953f75e4347ac5e349fbd3d2d0b94673c989
Samyuth <samyuth.sagi@gmail.com>
2022-04-07 16:41:46 -0400

    deployment db connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+const db = config.get('mongoURI_deployment');
 
 const connectDB = async () => {
     try {
