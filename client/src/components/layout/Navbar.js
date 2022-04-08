c67bd3d4fbe884a63db0b66e555c9b86b7b4921e
Omri Maor <omrimaor.rpi@gmail.com>
2022-04-08 16:51:34 -0400

    guestLink list commented out and added to return

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -24,44 +24,70 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
   const guestLinks = (
     <ul>
+        {/* <li>
+          <Link to="/login">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">About</Link>
+        </li>
         <li>
           <Link to="/register">Register</Link>
         </li>
         <li>
-          <Link to="/login">Login</Link>
-        </li>
+          <Link to="/login">Sign In</Link>
+        </li> */}
       </ul>
   );
 
     return (
-        <nav className="navbar bg-dark">
-      <h1>
-        <Link to="/">
-          <i className="fas fa-code"></i> Instantutor
-        </Link>
-      </h1>
-      { !loading && (<Fragment>{ isAuthenticated ? authLinks: guestLinks }</Fragment>)}
-    </nav>
+    //     <nav className="navbar bg-dark">
+    //   <h1>
+    //     <Link to="/">
+    //       <i className="fas fa-code"></i> Instantutor
+    //     </Link>
+    //   </h1>
+    //   { !loading && (<Fragment>{ isAuthenticated ? authLinks: guestLinks }</Fragment>)}
+    // </nav>
+
+
+<div className="navbar-collapse collapse">
+     <ul className="nav navbar-nav pull-right">
+        <li>
+          <Link to="/login">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">About</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Sign In</Link>
+        </li>
+      </ul>
+   </div>
+    
+   
 
 
     // fixed navbar
     // <div class="navbar navbar-inverse navbar-fixed-top headroom" >
 		//    <div class="container">
-		//        <div class="navbar-header"></div>
+		//        <div class="navbar-header">
     //          button
     // 				  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
     //          <a class="navbar-brand" href="insta_index.html"><img src="assetsNew/images/Instantutor.png" style="width: 64px"> Instantutor</a>
 		//      	</div> -- nav header
-    //     <div class="navbar-collapse collapse">
-		// 	      <ul class="nav navbar-nav pull-right">
-    //           <li class="active"><a href="#">Home</a></li>
-    //           <li><a href="insta_about.html">About</a></li>
-    //           <li><a href="insta_register.html">Register</a></li>
-    //           <li><a class="btn" href="insta_signin.html">Sign In</a></li>
-    //        </ul>
-    //    </div>  nav collapse
-//      </div> -- container
-//  </div>  -- navbar fixed
+    //        <div class="navbar-collapse collapse">
+		// 	         <ul class="nav navbar-nav pull-right">
+    //               <li class="active"><a href="#">Home</a></li>
+    //               <li><a href="insta_about.html">About</a></li>
+    //               <li><a href="insta_register.html">Register</a></li>
+    //               <li><a class="btn" href="insta_signin.html">Sign In</a></li>
+    //           </ul>
+    //        </div>  nav collapse
+//        </div> -- container
+//     </div>  -- navbar fixed
 
 
       // c1 <div className='' = "navbar">
