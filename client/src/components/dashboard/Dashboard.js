c40fabe08de21f1791f572a6d50642b63bc27854
ninheart <charlieliu1016@gmail.com>
2022-06-13 12:37:53 -0400

    dropdown button hides requests

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect, useState,setState } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
@@ -7,41 +7,87 @@ import { getRequestHistory } from '../../actions/request'
 import RequestItem from '../user-requests/UserRequestItem'
 import UserRequest from '../dashboard/UserRequest'
 
+
+
+//dropdown button
+/*
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className='btn'
+    >
+      {text}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  color: 'steelblue',
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+}
+*/
+
+
+
 const DashBoard = ({
   getRequestHistory,
   user,
   req_history = [],
   loading = true,
   match,
+  //showRequests, //show the drop down
+  
 }) => {
+  const [showRequests, setShowRequests] = useState(true) //for dropdown menu 
+  const dropDown = ()=>{
+    setShowRequests(!showRequests)
+  }
+
   useEffect(async () => {
     /*await (match.params.id ? 
             getRequestHistory(match.params.id) : 
             (user &&
             getRequestHistory(user._id)));*/
     ;(await user) && getRequestHistory(user._id)
-  }, [getRequestHistory, match.params.id, user])
+  }, [getRequestHistory, match.params.id, user]);
+  
   return (
+    //dropdown bar
+    //<Button/>
+
     <Fragment>
-      {loading ? (
-        <Spinner />
-      ) : req_history === null ||
-        req_history === undefined ||
-        req_history.length < 1 ? (
-        <div>
-          <h1 className='large text-primary'>Oops!</h1>
-          <h1 className='text-primary'>
-            Looks like u did not post any requests yet...
-          </h1>
-        </div>
-      ) : (
-        <div className='request'>
-          <h1 className='large text-primary'>DashBoard</h1>
-          {req_history.map((request) => (
-            <RequestItem key={request._id} item={request} />
-          ))}
-        </div>
-      )}
+      <h1 className='large text-primary'>DashBoard</h1>
+
+      <button onClick={()=>dropDown()}>
+        DropDownMenu
+      </button>
+      {showRequests ? 
+          (loading ? (
+            <Spinner />
+          ) : req_history === null ||
+            req_history === undefined ||
+            req_history.length < 1 ? (
+            <div>
+              <h1 className='large text-primary'>Oops!</h1>
+              <h1 className='text-primary'>
+                Looks like u did not post any requests yet...
+              </h1>
+            </div>
+          ) : (
+            <div className='request '>
+              {req_history.map((request) => (
+                <RequestItem key={request._id} item={request} />
+              ))}
+            </div>
+          )) : null
+      }
 
       {/* <Link to="/dashboard" className="btn btn-light">
                 Back to dashboard
@@ -64,6 +110,7 @@ const mapStateToProps = (state) => ({
   user: state.auth.user,
   req_history: state.user_requests.request_history,
   loading: state.user_requests.loading,
+  user_requests:state.user_requests,
 })
 
 export default connect(mapStateToProps, { getRequestHistory })(DashBoard)
